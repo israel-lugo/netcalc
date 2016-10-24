cad642390b7460c8757e242dbd189633a9c7ed57
Israel G. Lugo <israel.lugo@lugosys.com>
2016-10-24 03:13:13 +0100

    Allow command aliases for user.
    * commands.py (add_parser_compat): New function.

--- commands.py
+++ commands.py
@@ -33,6 +33,32 @@ import netaddr
 __all__ = [ 'command_registrators' ]
 
 
+def add_parser_compat(subparsers, *args, **kwargs):
+    """Add a parser to an argparse subparsers object.
+
+    Adds the parser in a Python 2/3 compatible manner. In particular,
+    Python 2 doesn't support the aliases keyword argument. Tries to use
+    this option it if possible, omit it otherwise.
+
+    """
+    try:
+        subparser = subparsers.add_parser(*args, **kwargs)
+
+    except TypeError as e:
+        # Failed. We're probably running on Python 2.x, which doesn't
+        # support the "aliases" keyword argument. Try again.
+        kwargs2 = kwargs.copy()
+        try:
+            del kwargs2['aliases']
+        except KeyError:
+            # there is no "aliases", original error was something else
+            raise e
+
+        subparser = subparsers.add_parser(*args, **kwargs2)
+
+    return subparser
+
+
 def network_address(string):
     """Convert a string to a network address, if possible.
 
@@ -54,13 +80,13 @@ def register_add(subparsers):
     Returns the subparser and the action function for this command.
 
     """
-    parser_add = subparsers.add_parser('add',
-                                       help="add networks, merging as much as possible")
+    subparser = add_parser_compat(subparsers, 'add',
+        help="add networks, merging as much as possible", aliases=["merge"])
 
-    parser_add.add_argument('networks', metavar='NETWORK', type=network_address,
-                            nargs='+', help="a network address")
+    subparser.add_argument('networks', metavar='NETWORK', type=network_address,
+        nargs='+', help="a network address")
 
-    return parser_add, do_add
+    return subparser, do_add
 
 
 def do_add(args):
