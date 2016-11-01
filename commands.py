ae11a02654421bfe9a80e7fa49960d7fad654091
Israel G. Lugo <israel.lugo@lugosys.com>
2016-11-01 14:01:09 +0000

    Make command examples relative to command module.

--- commands.py
+++ commands.py
@@ -120,7 +120,7 @@ class AddCommand(Command):
     Example:
       >>> parser = argparse.ArgumentParser()
       >>> subparsers = parser.add_subparsers()
-      >>> add = commands.AddCommand(subparsers)
+      >>> add = AddCommand(subparsers)
       >>> args = parser.parse_args("add 198.18.0.0/24 198.18.1.0/24 10.1/16 10/16".split())
       >>> args.func(args)
       10.0.0.0/15
@@ -156,7 +156,7 @@ class SubtractCommand(Command):
     Example:
       >>> parser = argparse.ArgumentParser()
       >>> subparsers = parser.add_subparsers()
-      >>> add = commands.AddCommand(subparsers)
+      >>> add = AddCommand(subparsers)
       >>> args = parser.parse_args("sub 192.0.2.0/24 192.0.2.0/28".split())
       >>> args.func(args)
       192.0.2.16/28
@@ -201,7 +201,7 @@ class ExprCommand(Command):
     Example:
       >>> parser = argparse.ArgumentParser()
       >>> subparsers = parser.add_subparsers()
-      >>> expr = commands.ExprCommand(subparsers)
+      >>> expr = ExprCommand(subparsers)
       >>> args = parser.parse_args("expr 10.16/13 - 10.20/14 + 10/12".split())
       >>> args.func(args)
       10.0.0.0/12
